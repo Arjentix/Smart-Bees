220b5aea08a3820d181dd29e17c4be5a8ea79b0f
Daniil <arjentix@gmail.com>
2019-05-03 12:52:53 +0300

    Added documentation and some iprovements

--- src/MQTTPublisher.cpp
+++ src/MQTTPublisher.cpp
@@ -12,6 +12,11 @@
 
 #define KEEP_ALIVE 120
 
+/*
+* This file defines class MQTTPublisher, described in MQTTPublisher.h.
+* See all methods documentations in the header file.
+*/
+
 MQTTPublisher::MQTTPublisher(std::string host, int port) : _host(host), _port(port)
 {
 	int res;
@@ -40,7 +45,28 @@ MQTTPublisher::MQTTPublisher(std::string host, int port) : _host(host), _port(po
 
 void MQTTPublisher::publish(std::string topic, std::string mes)
 {
-	mosquitto_publish(_mosq, NULL, topic.c_str(), mes.length(), mes.c_str(), 1, false);
+	int res;
+
+	res = mosquitto_publish(_mosq, NULL, topic.c_str(), mes.length(), mes.c_str(), 1, false);
+	if (res != MOSQ_ERR_SUCCESS) {
+		switch (res) {
+		case MOSQ_ERR_INVAL:
+			throw std::runtime_error("MQTT publishing failed cause of invalid parameters");
+			break;
+		case MOSQ_ERR_NOMEM:
+			throw std::runtime_error("MQTT publishing failed cause of out of memory");
+			break;
+		case MOSQ_ERR_NO_CONN:
+			throw std::runtime_error("MQTT publishing failed cause of no connection to the broker");
+			break;
+		case MOSQ_ERR_PROTOCOL:
+			throw std::runtime_error("MQTT publishing failed cause of protocol error communticating with the broker");
+			break;
+		case MOSQ_ERR_PAYLOAD_SIZE:
+			throw std::runtime_error("MQTT publishing failed cause of payload is too large");
+			break;
+		}
+	}
 }
 
 MQTTPublisher::~MQTTPublisher()
--- src/MQTTPublisher.h
+++ src/MQTTPublisher.h
@@ -5,6 +5,14 @@
 * Date: 02.0.19
 */
 
+#ifndef MQTT_PUBLISHER_H
+#define MQTT_PUBLISHER_H
+
+/*
+* This file describes MQTTPublisher class, which can publish messages in the given topic.
+* It connects to the broker on host and port provided in conctructor.
+*/
+
 #include <mosquitto.h>
 #include <string>
 
@@ -12,11 +20,22 @@ class MQTTPublisher {
 private:
 	std::string		_host;
 	int			_port;
-	struct mosquitto	*_mosq;
+	struct mosquitto	*_mosq; 	// Mosquitto instance for API
 
 public:
+	/*
+	* MQTTPublisher() - gets broker's host and port (usually 1883) and connects to it.
+	* Can throw std::runtime_error if can't create new mosquitto instance or can't connect to server.
+	*/
 	MQTTPublisher(std::string host, int port);
+
 	~MQTTPublisher();
 
+	/*
+	* publish() - publish given message to the given topic with quality = 1 and not retain.
+	* Cat thow std::runtime_error if some error occured in publishing process.
+	*/
 	void publish(std::string topic, std::string mes);
 };
+
+#endif // MQTT_PUBLISHER_H
