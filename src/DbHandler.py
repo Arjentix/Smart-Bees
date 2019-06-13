823cde8e897cf44b179e919ff648ab5a08d58f1f
Daniil <arjentix@gmail.com>
2019-06-14 01:11:09 +0300

    Deletes extra method

--- src/DbHandler.py
+++ src/DbHandler.py
@@ -17,21 +17,12 @@ class DbHandler:
 		# Closing connection to Db
 		self.connection.close()
 
-	def bind_gate_uid(self, gate, uid):
-		# Pushing new data to Db
-		with self.connection.cursor() as cursor:
-			data = (gate, uid)
-			query = 'INSERT INTO gate_uid(gate, uid) VALUES (%s, %s)'
-			cursor.execute(query, data)
-			self.connection.commit()
-
-			return True
-
 	def get_gate_from_db(self, user_id):
 		with self.connection.cursor() as cursor:
 			query = 'SELECT gate FROM gate_uid WHERE uid = \'{}\''.format(user_id)
 			cursor.execute(query)
 			res = cursor.fetchone()
+			logging.info('Result from db: %s', res)
 			if res is not None:
 				return res['gate']
 			else:
