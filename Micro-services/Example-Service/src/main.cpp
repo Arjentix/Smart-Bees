9a5a274980d0e259f5b29d74064ea97912bfc45a
Daniil <arjentix@gmail.com>
2019-09-28 16:37:10 +0300

    Updated for using SimpleSQL lib

--- Micro-services/Example-Service/src/main.cpp
+++ Micro-services/Example-Service/src/main.cpp
@@ -5,7 +5,8 @@
  * @date: 27.09.2019
 */
 
-#include <mysql.h>
+#include "SimpleSQL.h"
+
 #include <unistd.h>
 #include <iostream>
 
@@ -19,51 +20,39 @@
 using namespace std;
 
 int main() {
-	cout << "Start" << endl;
+	try {
+		cout << "Start" << endl;
 
-	MYSQL* conn = mysql_init(NULL);
-	if (conn == NULL) {
-		cout << "Initialization failed" << endl;
-		exit(-1);
-	}
+		SimpleSQL::Connector db;
 
-	MYSQL* connect_res;
-	for (int i = 0; i < ATTEMPTS_COUNT; ++i) {
-		cout << "Trying to connect: attempt #" << i + 1 << endl;
-		connect_res = mysql_real_connect(conn, HOST, USER_NAME, PASSWORD, DB_NAME, PORT, NULL, 0);
-		if (connect_res != NULL) {
-			break;
+		for (int i = 0; i < ATTEMPTS_COUNT; ++i) {
+			cout << "Trying to connect: attempt #" << i + 1 << endl;
+			db.connect(HOST, USER_NAME, PASSWORD, DB_NAME, PORT);
+			if (db.is_connected()) {
+				break;
+			}
+
+			sleep(1);
 		}
-		sleep(1);
+		if (!db.is_connected()) {
+			cout << "Connection failed after " << ATTEMPTS_COUNT << " attempts."
+				<< endl;
+			exit(-2);
+		}
+
+		// Writing new data
+		db.query("INSERT INTO test_table(Data) VALUES(\"Generated data\")");
+
+		// Reading last data
+		auto query_res_ptr = db.query("SELECT * FROM test_table ORDER BY Id DESC;");
+		SimpleSQL::Row row = query_res_ptr->get_row();
+
+		cout << "Id: " << row[0] << ", data: " << row[1] << endl;
+
+		cout << "End" << endl;
+		return 0;
 	}
-	if (connect_res == NULL) {
-		cout << "Connection failed after " << ATTEMPTS_COUNT << " attempts. "
-				<< mysql_error(connect_res) << endl;
-		exit(-2);
+	catch(exception& ex) {
+		cout << ex.what() << endl;
 	}
-
-	// Writing new data
-	int query_res = mysql_query(conn, "INSERT INTO test_table(Data) VALUES(\"Generated data\")");
-	if (query_res != 0) {
-		cout << "Query failed: " << mysql_error(conn) << endl;
-		exit(-4);
-	}
-
-	// Reading last data
-	query_res = mysql_query(conn, "SELECT * FROM test_table ORDER BY Id DESC;");
-	if (query_res != 0) {
-		cout << "Query failed: " << mysql_error(conn) << endl;
-		exit(-4);
-	}
-
-	MYSQL_RES* mysql_res = mysql_use_result(conn);
-	MYSQL_ROW mysql_row = mysql_fetch_row(mysql_res);
-
-	cout << "Id: " << mysql_row[0] << ", data: " << mysql_row[1] << endl;
-
-	mysql_free_result(mysql_res);
-	mysql_close(conn);
-
-	cout << "End" << endl;
-	return 0;
 }
