daba2f460589b7ebef6de0a646b2ad616dad5cfd
Daniil <arjentix@gmail.com>
2019-10-20 23:16:51 +0300

    Added handling gate answer

--- Micro-services/Active-Connections-Service/src/RequestHandlers/SendCommandRequestHandler/SendCommandRequestHandler.cpp
+++ Micro-services/Active-Connections-Service/src/RequestHandlers/SendCommandRequestHandler/SendCommandRequestHandler.cpp
@@ -21,10 +21,33 @@ HTTPHandler::Answer SendCommandRequestHandler::handle(HTTPHandler::Request reque
 	auto json_body = json::parse(request.body);
 
 	try {
-		_connection_manager.send_command(
+		string gate_answer = _connection_manager.send_command(
 			json_body.at("gate_id").get<string>(),
 			json_body.at("command").get<string>()
 		);
+		logger << "Gate answer = '" << gate_answer << "'" << endl;
+		HTTPHandler::Answer http_answer;
+		if (gate_answer == "OK") {
+			http_answer = {
+				200, "OK",
+				{
+					{"Content-Length", "0"},
+					{"Connection", "close"}
+				},
+				""
+			};
+		}
+		else {	// Gate can't process this command
+			http_answer = {
+				421, "Misdirected Request",
+				{
+					{"Content-Length", "0"},
+					{"Connection", "close"}
+				},
+				""
+			};
+		}
+		return http_answer;
 	}
 	catch (out_of_range& ex) {
 		return {
@@ -47,6 +70,17 @@ HTTPHandler::Answer SendCommandRequestHandler::handle(HTTPHandler::Request reque
 			""
 		};
 	}
+	catch (HTTPServer::RecvFailed& ex) {
+		logger << "SendCommandRequestHandler::Recv failed: " << ex.what() << endl;
+		return {
+			503, "Service Unavailable",
+			{
+				{"Content-Length", "0"},
+				{"Connection", "close"}
+			},
+			""
+		};
+	}
 	catch (exception& ex) {
 		logger << "SendCommandRequestHandler::Unexcpected exception: " << ex.what() << endl;
 		return {
@@ -58,12 +92,4 @@ HTTPHandler::Answer SendCommandRequestHandler::handle(HTTPHandler::Request reque
 			""
 		};
 	}
-	return {
-		200, "OK",
-		{
-			{"Content-Length", "0"},
-			{"Connection", "close"}
-		},
-		""
-	};
 }
