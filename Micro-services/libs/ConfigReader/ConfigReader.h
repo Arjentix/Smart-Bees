444750df9508fa2ca921cd3a06d9ce2bac6ed2d2
kiri3L <klubimoff@gmai.com>
2019-11-06 15:17:15 +0300

    CMakeLists update. Rename method ReadValueByKey -> read_value_by_key

--- Micro-services/libs/ConfigReader/ConfigReader.h
+++ Micro-services/libs/ConfigReader/ConfigReader.h
@@ -9,11 +9,11 @@
  *      ConfigReader::ConfigReader reader("config_file.txt")
  *      reader.read_config() // throws std::ios_base::failure
  *
- *      int variable_name1 = reader.ReadValueByKey<int>("variable_name1")
- *      float variable_name2 = reader.ReadValueByKey<float>("variable_name2")
- *      double variable_name3 = reader.ReadValueByKey<double>("variable_name3")
- *      bool variable_name4 = reader.ReadValueByKey<bool>("variable_name4")
- *      std::string variable_name5 = reader.ReadValueByKey<std::string>("variable_name5")
+ *      int variable_name1 = reader.read_value_by_key<int>("variable_name1")
+ *      float variable_name2 = reader.read_value_by_key<float>("variable_name2")
+ *      double variable_name3 = reader.read_value_by_key<double>("variable_name3")
+ *      bool variable_name4 = reader.read_value_by_key<bool>("variable_name4")
+ *      std::string variable_name5 = reader.read_value_by_key<std::string>("variable_name5")
  *
  *  } catch (std::invalid_argument e) {
  *      std::cout << e.what()
@@ -35,11 +35,9 @@
  *
  */
 
-#include <stdexcept>
-
-
 #pragma once
 
+#include <stdexcept>
 #include <string>
 #include <map>
 #include <fstream>
@@ -53,55 +51,17 @@ namespace ConfigReader{
         T operator()(const std::string &s){
             return T(s);
         };
-
     };
 
-    template<> int TypeParser<int>::operator()( const std::string &s){
-        std::regex r("\\s*-?[0-9]+\\s*");
-        if(std::regex_match(s,r)){
-            return atoi(s.c_str());
-        }
-        throw std::invalid_argument(s);
-    }
+    template <> int TypeParser<int>::operator()(const std::string &s);
 
-    template <> float TypeParser<float>::operator()( const std::string &s){
-        std::regex r("\\s*[-]?[0-9]+\\.[0-9]*\\s*");
-        if(std::regex_match(s,r)){
-            return atof(s.c_str());
-        }
-        throw std::invalid_argument(s);
-    }
+    template <>
+    float TypeParser<float >::operator()(const std::string &s);
 
-    template <> double TypeParser<double>::operator()(const std::string &s){
-        std::regex r("\\s*-?[0-9]+\\.[0-9]*\\s*");
-        if(std::regex_match(s,r)){
-            return atof(s.c_str());
-        }
-        throw std::invalid_argument(s);
-    }
+    template <>
+    bool TypeParser<bool >::operator()(const std::string &s);
 
-    template <> bool TypeParser<bool>::operator()(const std::string &s){
-        std::regex r_true("\\s*true\\s*");
-        std::regex r_false("\\s*false\\s*");
-        if(std::regex_match(s,r_true)){
-            return true;
-        } else if(std::regex_match(s,r_false)){
-            return false;
-        }
-        throw std::invalid_argument(s);
-    }
-
-    template <> std::string TypeParser<std::string>::operator()(const std::string &s){
-        std::regex r1("\\s*\\w+\\s*");
-        std::regex r2("\\s*\"[^\"]*\"\\s*");
-        if(std::regex_match(s,r1)){
-            std::regex r("\\s*");
-            return std::regex_replace(s,r,"");
-        } else if(std::regex_match(s,r2)){
-            return s.substr(s.find("\"") + 1,s.rfind("\"") - s.find("\"") - 1);
-        }
-        throw std::invalid_argument(s);
-    }
+    template <> std::string TypeParser<std::string>::operator()(const std::string &s);
 
     class ConfigReader{
     private:
@@ -110,6 +70,10 @@ namespace ConfigReader{
     public:
 
         ConfigReader(std::string file_name){
+            this->_file_name = file_name;
+        }
+
+        void set_file_name(std::string file_name){
             this->_file_name = file_name;
         }
 
@@ -130,20 +94,26 @@ namespace ConfigReader{
                     }
 
                 }
-            } input.exceptions(std::ifstream::failbit);
+            }
+            else{
+                input.exceptions(std::ifstream::failbit);
+            }
         }
 
         template < typename T>
-        T ReadValueByKey(std::string key){
+        T read_value_by_key(std::string key){
             std::map<std::string,std::string>::iterator it = this->variables.find(key);
             if(it != variables.end()){
                 TypeParser<T> typeParser;
                 return typeParser((*it).second);
             } else {
-                throw std::invalid_argument(key);
+                throw std::invalid_argument("argument not found: " + key);
             }
 
         }
     };
+
+    extern ConfigReader reader;
 }
+
 
