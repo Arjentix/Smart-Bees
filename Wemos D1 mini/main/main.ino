13a3206fe4658871050a1229acb085e74c6b2dfc
Arjentix <arjentix@gmail.com>
2019-11-06 13:49:59 +0300

    Added blink and print functions

--- Wemos D1 mini/main/main.ino
+++ Wemos D1 mini/main/main.ino
@@ -21,11 +21,12 @@
 #include <PubSubClient.h>
 #include <SoftwareSerial.h>
 #ifndef STASSID
-#define STASSID "****"
-#define STAPSK  "****"
+#define STASSID "******"
+#define STAPSK  "******"
 #endif
 
 #define PORT 1883
+#define DEBUG 0
 
 /*
 * callback() - is calling when a message from topic was received.
@@ -42,25 +43,32 @@ void broker_connect();
 */
 void mactopic_subscribe();
 
+/*
+ * print() - prints given string if DEBUG is set to 1
+ */
+void print(String str);
+
+void blink();
+
 const char* ssid     = STASSID;
 const char* password = STAPSK;
-byte broker[] = {192, 168, 0, 19};
+int debug_flag = DEBUG;
+byte broker[] = {192, 168, 43, 33};
 char mac_str[17];
 WiFiClient espClient;
 PubSubClient client(espClient);
-//SoftwareSerial s(3, 1);
-
 int first_time = true;
 
 void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);
+  char log_mes[256];
   Serial.begin(9600);
 
   // Start by connecting to a WiFi network
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  print("");
+  print("");
+  sprintf(log_mes, "Connecting to %s", ssid);
+  print(log_mes);
 
   /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
      would try to act as both a client and an access-point and could cause
@@ -70,13 +78,14 @@ void setup() {
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    print(".");
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  print("");
+  print("WiFi connected");
+  print("IP address: ");
+  memcpy(log_mes, WiFi.localIP(), 256);
+  print(log_mes);
 
   randomSeed(micros());
 
@@ -98,51 +107,46 @@ void loop() {
 
 void callback(char* topic, byte* payload, unsigned int length)
 {
+  blink();
   char log_mes[256];
   char p[length + 1];
 
   if (first_time) {
-    Serial.println("Initial callback started");
+    print("Initial callback started");
 
     memcpy(p, payload, length);
     p[length] = '\0';
 
     sprintf(log_mes, "Unsubscribing from %s", mac_str);
-    Serial.println(log_mes);
+    print(log_mes);
     client.unsubscribe(mac_str);
 
     sprintf(log_mes, "Subscribing to the \"%s\"", p);
-    Serial.println(log_mes);
+    print(log_mes);
     client.subscribe(p);
 
     first_time = false;
   }
   else {
-    Serial.println("Callback!");
+    print("Callback!");
 
     memcpy(p, payload, length);
     p[length] = '\0';
 
     sprintf(log_mes, "Getted %s", p);
-    Serial.println(log_mes);
-    Serial.println(p);
-   // s.println(p);
+    print(log_mes);
+    Serial.print(p);
   }
 }
 
 void broker_connect()
 {
   char log_mes[256];
-//  client.setServer("test.mosquitto.com", PORT);
-//  while(!client.connected()) {
-//    Serial.println("Trying connect to a broker");
-//    client.connect("Beeclient");
-//  }
 
   // Connecting to the broker
   for(;; client.setServer(broker, PORT)) {
     sprintf(log_mes, "Trying %d.%d.%d.%d on port %d...", broker[0], broker[1], broker[2], broker[3], PORT);
-    Serial.println(log_mes);
+    print(log_mes);
     
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
@@ -150,15 +154,14 @@ void broker_connect()
 
     if (client.connected()) {
       sprintf(log_mes, "Connected to the broker %d.%d.%d.%d on port %d", broker[0], broker[1], broker[2], broker[3], PORT);
-      Serial.println(log_mes);
+      print(log_mes);
       break;
     }
     else {
-      Serial.print("failed, rc=");
-      Serial.println(client.state());
+      print("failed, rc=" + client.state());
 //      if (broker[2] == 255) {
 //        sprintf(log_mes, "Can't find any broker on port %d", PORT);
-//        Serial.println(log_mes);
+//        print(log_mes);
 //        break;
 //      }
 //      else if (broker[3] == 255) {
@@ -179,6 +182,23 @@ void mactopic_subscribe()
   WiFi.macAddress(mac);
   sprintf(mac_str, "%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   sprintf(log_mes, "Subscribing to the topic \"%s\"", mac_str);
-  Serial.println(log_mes);
+  print(log_mes);
   client.subscribe(mac_str);
+}
+
+void print(String str)
+{
+  if (debug_flag == 1) {
+    Serial.println(str);
+  }
+}
+
+void blink()
+{
+  for (int i = 0; i < 2; ++i) {
+    digitalWrite(BUILTIN_LED, HIGH);
+    delay(400);
+    digitalWrite(BUILTIN_LED, LOW);
+    delay(400);
+  }
 }
