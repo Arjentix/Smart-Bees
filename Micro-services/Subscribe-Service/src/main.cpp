2edc56d67d53e22c3d0cd223fcd018ce46854794
glofst <forglofst@gmail.com>
2019-12-08 21:39:46 +0300

    fixed and added threads

--- Micro-services/Subscribe-Service/src/main.cpp
+++ Micro-services/Subscribe-Service/src/main.cpp
@@ -6,9 +6,14 @@
 #include "ConfigReader.h"
 
 #include <unistd.h>
-#include <signal.h>
-#include <iostream>
 #include <sstream>
+#include <unistd.h>
+#include <signal.h>
+#include <vector>
+#include <future>
+#include <thread>
+#include <iostream>
+
 
 using namespace std;
 using json = nlohmann::json;
@@ -39,27 +44,17 @@ HTTPHandler::Answer work_with_db(DataBase& db, HTTPHandler::Request const& reque
 	struct tm tm;
 	json args_json;
 	json answer_json;
-	int user_id;
-	string command;
 
 
 	try {
 		check_for_api_key(request.headers.at("Api-Key"));
 
-		if(request.method == HTTPHandler::Method::POST || request.method == HTTPHandler::Method::PUT)
-			args_json = json::parse(request.body);
-		else {
-			user_id = stoi(request.variables.at("user_id"));
-			if(request.method == HTTPHandler::Method::GET)
-				command = request.uri;
-		}
-
 		switch(request.method) {
 			case HTTPHandler::Method::GET:
-				if(command == "/sub_status")
-					answer_json["sub_status"] = db.check_for_sub(user_id);
-				else if(command == "/sub_left") {
-					DataBase::Time t_r = db.time_left(user_id);
+				if(request.uri == "/sub_status")
+					answer_json["sub_status"] = db.check_for_sub(args_json["user_id"].get<string>());
+				else if(request.uri == "/sub_left") {
+					DataBase::Time t_r = db.time_left(args_json["user_id"].get<string>());
 					answer_json["minutes"] = t_r.minutes;
 					answer_json["hours"] = t_r.hours;
 					answer_json["days"] = t_r.days;
@@ -68,19 +63,21 @@ HTTPHandler::Answer work_with_db(DataBase& db, HTTPHandler::Request const& reque
 					throw runtime_error("Unknown command");
 				break;
 			case HTTPHandler::Method::PUT:
+				args_json = json::parse(request.body);
 				db.update_sub(args_json["user_id"].get<string>(), 
 							  args_json["sub_start_date"].get<string>(), 
 							  args_json["sub_end_date"].get<string>()
 							 );
 				break;
 			case HTTPHandler::Method::POST:
+				args_json = json::parse(request.body);
 				db.insert_sub(args_json["user_id"].get<string>(), 
 							  args_json["sub_start_date"].get<string>(), 
 							  args_json["sub_end_date"].get<string>()
 							 );
 				break;
 			case HTTPHandler::Method::DELETE:
-				db.delete_sub(user_id);
+				db.delete_sub(args_json["user_id"].get<string>());
 				break;
 			default:
 				throw runtime_error("Unknown HTTP Method");
@@ -93,26 +90,17 @@ HTTPHandler::Answer work_with_db(DataBase& db, HTTPHandler::Request const& reque
 		set_bad_request(answer);
 	   	answer_json["error_message"] = e.what();
 	}
-
+	
 	answer.body = answer_json.dump(4);
+	answer.headers.insert(pair<string, string>("Content-Type", "application/json"));
+	answer.headers.insert(pair<string, string>("Content-Length", to_string(answer.body.size())));
+	answer.headers.insert(pair<string, string>("Connecton", "close"));
 	return answer;
 }
 
 string request_to_str(HTTPHandler::Request const& request) {
 	stringstream result_ss;
 	HTTPHandler::write_request(request, result_ss);
-/*	result_ss << request.method << " " 
-			  << request.uri << " HTTP/1.1" << endl;
-	for(auto it = request.headers.begin(); it != request.headers.end();++it)
-		result_ss << it->first << ": " << it->second << endl;
-	result_ss << endl;
-
-	for(auto it = request.variables.begin(); it != request.variables.end();++it)
-		result_ss << it->first << " = " << it->second << endl;
-	result_ss << endl;
-
-	result_ss << request.body;
-*/
 	return result_ss.str();
 }
 
@@ -126,6 +114,35 @@ string answer_to_str(HTTPHandler::Answer const& answer) {
 void signal_handler(int) {
 	finish = true;
 }
+
+//DataBase db;
+
+void handle_client(int client_sock, DataBase db)
+{
+	HTTPHandler::Answer answer;
+
+	try {
+		HTTPHandler::Request request = HTTPServer::get_request(client_sock);
+		logger << "Request:\n" << request_to_str(request) << endl;
+
+		answer = work_with_db(db, request);
+
+		logger << "Answer:\n" << answer_to_str(answer) << endl;
+		HTTPServer::send_answer(client_sock, answer);
+		logger << "Answer was sent" << endl;
+
+	} catch (HTTPServer::ClientDisconnected& ex) {
+		logger << ex.what() << endl;
+		HTTPServer::close_con(client_sock);
+		logger << "Connection closed with client id:" << client_sock << endl;
+		return;
+	}
+
+	HTTPServer::close_con(client_sock);
+	logger << "Connection closed with client id:" << client_sock << endl;
+}
+
+
 
 int main(int argc, char** argv) {
 
@@ -149,6 +166,8 @@ int main(int argc, char** argv) {
 
 	try {
 
+		// Capturing SIGINT signal
+		signal(SIGINT, signal_handler);
 		ConfigReader::reader.set_file_name("config.txt");
 		ConfigReader::reader.read_config(); // Buffering all configs
 
@@ -157,7 +176,7 @@ int main(int argc, char** argv) {
 		logger << "SubService starting..." << endl;
 
 		logger << "Initating database..." << endl;
-   		DataBase db;
+		DataBase db;
 		db.init(
 				ConfigReader::reader.read_value_by_key<string>("DB_HOSTNAME").c_str(),
 				ConfigReader::reader.read_value_by_key<string>("DB_NAME").c_str(),
@@ -171,25 +190,16 @@ int main(int argc, char** argv) {
 		server.start_server(ConfigReader::reader.read_value_by_key<int>("HTTP_SERVER_PORT"));
 		server.turn_to_listen(10);
 
-		// Capturing SIGINT signal
-		signal(SIGINT, signal_handler);
 
-		while(!finish) {
+		vector<future<void>> futures;
+
+		while(!server.is_interrupted()) {
 			logger << "Awaiting for connection..." << endl;
-			int client_id = server.connect_client();
-			logger << "Client with id: " << client_id << " was connected" << endl;
-
-			HTTPHandler::Request request = server.get_request(client_id);
-			logger << "Request:\n" << request_to_str(request) << endl;
-
-			HTTPHandler::Answer answer = work_with_db(db, request);
-			logger << "Answer:\n" << answer_to_str(answer) << endl;
-			server.send_answer(client_id, answer);
-			logger << "Answer was sent" << endl;
-
-			server.close_con(client_id);
-			logger << "Connection closed with client id: " << client_id << endl;
+			int client_sock = server.connect_client();
+			logger << "Client with id: " << client_sock << " was connected" << endl;
+			futures.push_back(async(handle_client, client_sock, db));
 		}
+
     } catch (exception& e) {
         logger << "[EXCEPTION] " << e.what() << endl;
     }
